6c09f397062fc404bd0c587b1d3503a1c3d6376a
friggito <ornella.friggit@gmail.com>
2019-03-15 16:37:55 -0400

    feat(input-form): refactoring evening form for DRY

--- client/components/evening-form.js
+++ client/components/evening-form.js
@@ -11,82 +11,51 @@ class EveningForm extends Component {
 
   handleCheck() {}
   render() {
+    const radioButtonsRow = (
+      <div className="row">
+        <form>
+          {this.state.counterTen.map(num => (
+            <div className="col s1" key={num}>
+              <label>
+                <input
+                  name="group1"
+                  type="radio"
+                  value={num}
+                  onClick={this.handleCheck}
+                />
+                <span>{num}</span>
+              </label>
+            </div>
+          ))}
+        </form>
+      </div>
+    )
+
     return (
       <div>
         Rate the overall pleasantness of your day:
-        <div className="row">
-          <form>
-            {this.state.counterTen.map(num => (
-              <div className="col s1">
-                <label>
-                  <input
-                    name="group1"
-                    type="radio"
-                    checked
-                    value={num}
-                    handleClick={this.handleCheck}
-                  />
-                  <span>{num}</span>
-                </label>
-              </div>
-            ))}
-          </form>
-        </div>
+        {radioButtonsRow}
         Rate the tension in your day ( excitement is a postive tension. low
         energy high tension: dread. low pleasatness, high plesasantness: excited
         low energy low tension: calmness high pleasantness. tiredness/bored low
         pleasantness )?
-        <div className="row">
-          <form>
-            {this.state.counterTen.map(num => (
-              <div className="col s1">
-                <label>
-                  <input
-                    name="group1"
-                    type="radio"
-                    checked
-                    value={num}
-                    handleClick={this.handleCheck}
-                  />
-                  <span>{num}</span>
-                </label>
-              </div>
-            ))}
-          </form>
-        </div>
+        {radioButtonsRow}
         Rate your general energy level for today (low energy unpleasant: tired.
         high energy unpeasnt: anxious low energy pleasnat: calm. high energy
         pleasant: ecited)
-        <div className="row">
-          <form>
-            {this.state.counterTen.map(num => (
-              <div className="col s1">
-                <label>
-                  <input
-                    name="group1"
-                    type="radio"
-                    checked
-                    value={num}
-                    handleClick={this.handleCheck}
-                  />
-                  <span>{num}</span>
-                </label>
-              </div>
-            ))}
-          </form>
-        </div>
+        {radioButtonsRow}
         <form>
           <label htmlFor="feelings">Can you journal in some thoughts?</label>
           <input
             type="text"
             name="journalEntry"
-            // value={this.state.journalEntry}
-            // onChange={this.handleChange}
+            value={this.state.journalEntry}
+            onChange={this.handleChange}
           />
           <button
             className="waves-effect waves-light btn-large"
             type="button"
-            // onClick={this.handleSubmit}
+            onClick={this.handleSubmit}
           >
             Submit
           </button>
