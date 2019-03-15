8adb200a68f504e7fb84e6b237e4f7f7e30784d7
friggito <ornella.friggit@gmail.com>
2019-03-15 17:43:52 -0400

    feat(models): Edited morningEntries model

--- server/db/models/morningEntries.js
+++ server/db/models/morningEntries.js
@@ -2,10 +2,58 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const morningEntry = db.define('morningEntry', {
-  email: {
+  sleep: {
     type: Sequelize.STRING,
-    unique: true,
+    allowNull: false,
+    validate: {
+      isIn: [['0-2', '2-4', '4-6', '6-8', '8+']]
+    }
+  },
+  social: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isIn: [
+        ['More than usual', 'Usual amount', 'Less than usual', 'Not at all']
+      ]
+    }
+  },
+  sun: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0,
+      max: 5
+    }
+  },
+  relax: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isIn: [
+        ['More than usual', 'Usual amount', 'Less than usual', 'Not at all']
+      ]
+    }
+  },
+  exercise: {
+    type: Sequelize.BOOLEAN,
     allowNull: false
+  },
+  work: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0,
+      max: 5
+    }
+  },
+  meals: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0,
+      max: 4
+    }
   }
 })
 
