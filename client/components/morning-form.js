bffa49ad6493a56d203119caf4cbd084ea12bcf3
friggito <ornella.friggit@gmail.com>
2019-03-16 17:04:27 -0400

    feat(input-form): set up handleCheck function

--- client/components/morning-form.js
+++ client/components/morning-form.js
@@ -4,68 +4,57 @@ import {connect} from 'react-redux'
 import {postMorningEntry} from '../store'
 
 class PlanningForm extends Component {
-  constructor() {
-    super()
-    this.state = {
-      counterTen: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-      counterFive: [0, 1, 2, 3, 4, 5],
-      mealCounter: [0, 1, 2, 3, 4],
-      binaryCounter: ['Yes', 'No'],
-      usualCounter: [
-        'Not at all',
-        'Less than usual',
-        'The usual amount',
-        'More than usual'
-      ],
-      sleepHours: ['0-2', '2-4', '4-6', '6-8', '8+']
-    }
+  constructor(props) {
+    super(props)
     this.handleCheck = this.handleCheck.bind(this)
   }
-
-  handleCheck() {}
+  handleCheck(event) {
+    console.log('this triggered the handleCheck')
+    this.props.currentMorningEntry[event.target.inputname] = event.target.value
+  }
   render() {
     return (
       <div>
         How many hours of sleep did you get?
         <RadioButtonsRow
-          counter={this.state.sleepHours}
-          handleClick={this.handleCheck}
+          counterType="sleepHours"
+          clickHandler={this.handleCheck}
           name="sleep"
         />
         How much did you socialize?
         <RadioButtonsRow
-          counter={this.state.usualCounter}
-          handleClick={this.handleCheck}
+          counterType="usualCounter"
+          clickHandler={this.handleCheck}
           name="social"
         />
         How many meals did you eat?
         <RadioButtonsRow
-          counter={this.state.mealCounter}
-          handleClick={this.handleCheck}
+          counterType="mealCounter"
+          clickHandler={this.handleCheck}
           name="meals"
         />
         Did you exercise?
         <RadioButtonsRow
-          counter={this.state.binaryCounter}
-          handleClick={this.handleCheck}
+          counterType="binaryCounter"
+          clickHandler={this.handleCheck}
           name="exercise"
         />
         What is your outlook on work today (0 being worst to 5 being best)?
         <RadioButtonsRow
-          counter={this.state.counterFive}
-          handleClick={this.handleCheck}
+          counterType="counterFive"
+          clickHandler={this.handleCheck}
           name="work"
         />
         How much did you relax today?
         <RadioButtonsRow
-          counter={this.state.usualCounter}
-          handleClick={this.handleCheck}
+          counterType="usualCounter"
+          clickHandler={this.handleCheck}
           name="relax"
         />
         How sunny was it today (0 being gloomy to 5 being sunniest)?
         <RadioButtonsRow
-          counter={this.state.counterFive}
-          handleClick={this.handleCheck}
+          counterType="counterFive"
+          clickHandler={this.handleCheck}
           name="sun"
         />
       </div>
--- client/components/radio-buttons-row.js
+++ client/components/radio-buttons-row.js
@@ -1,17 +1,31 @@
 import React from 'react'
 
 const RadioButtonsRow = props => {
+  const counter = {
+    counterTen: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    counterFive: [0, 1, 2, 3, 4, 5],
+    mealCounter: [0, 1, 2, 3, 4],
+    binaryCounter: ['Yes', 'No'],
+    usualCounter: [
+      'Not at all',
+      'Less than usual',
+      'The usual amount',
+      'More than usual'
+    ],
+    sleepHours: ['0-2', '2-4', '4-6', '6-8', '8+']
+  }
   return (
     <div className="row">
       <form>
-        {props.counter.map(num => (
+        {counter[props.counterType].map(num => (
           <div className="col s1" key={num}>
             <label>
               <input
+                inputname={props.name}
                 name="group1"
                 type="radio"
                 value={num}
-                onClick={props.handleClick}
+                onClick={props.clickHandler}
               />
               <span>{num}</span>
             </label>
