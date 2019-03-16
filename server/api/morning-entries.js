6d311d9207961bed85f5725c53a9121fd1bd83a6
friggito <ornella.friggit@gmail.com>
2019-03-16 17:24:57 -0400

    fix(morning-entries api route): correct post route

--- server/api/morning-entries.js
+++ server/api/morning-entries.js
@@ -2,10 +2,9 @@ const router = require('express').Router()
 const {MorningEntry} = require('../db/models')
 module.exports = router
 
-router.put('/', async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   try {
-    console.log('\n *** REQ.BODY: *** \n', req.body)
-    const newMorningEntry = MorningEntry.create(req.body)
+    const newMorningEntry = await MorningEntry.create(req.body)
     res.send(newMorningEntry)
   } catch (error) {
     next(error)
