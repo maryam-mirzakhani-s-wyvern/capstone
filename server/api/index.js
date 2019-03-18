a96b9e2d5e74545dd76783eb72b6c515b2470fc3
Dani Jordan <dandelionwalk@gmail.com>
2019-03-18 15:20:30 -0400

    Today (#7)
    * made some changes for the frontend
    
    * feat(today): added today view, changed name/titles

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,7 @@ module.exports = router
 
 router.use('/users', require('./users'))
 router.use('/morning-entries', require('./morning-entries'))
+// router.use('/weather', require('./weather'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
