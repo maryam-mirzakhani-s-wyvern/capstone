91152e3338c2cd795b4d87a31b4301b5a9d6d62d
friggito <ornella.friggit@gmail.com>
2019-03-18 17:23:48 -0400

    feat(evening-form): added more questions

--- client/components/evening-form.js
+++ client/components/evening-form.js
@@ -5,7 +5,6 @@ class EveningForm extends Component {
   constructor() {
     super()
     this.state = {
-      counterTen: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
       journalEntry: ''
     }
     this.handleCheck = this.handleCheck.bind(this)
@@ -15,22 +14,62 @@ class EveningForm extends Component {
   render() {
     return (
       <div>
+        How many hours of sleep did you get?
+        <RadioButtonsRow
+          counterType="sleepHours"
+          clickHandler={this.handleCheck}
+          name="sleep"
+        />
+        How much did you socialize?
+        <RadioButtonsRow
+          counterType="usualCounter"
+          clickHandler={this.handleCheck}
+          name="social"
+        />
+        How many meals did you eat?
+        <RadioButtonsRow
+          counterType="mealCounter"
+          clickHandler={this.handleCheck}
+          name="meals"
+        />
+        Did you exercise?
+        <RadioButtonsRow
+          counterType="binaryCounter"
+          clickHandler={this.handleCheck}
+          name="exercise"
+        />
+        What is your outlook on work today (0 being worst to 5 being best)?
+        <RadioButtonsRow
+          counterType="counterFive"
+          clickHandler={this.handleCheck}
+          name="work"
+        />
+        How much did you relax today?
+        <RadioButtonsRow
+          counterType="usualCounter"
+          clickHandler={this.handleCheck}
+          name="relax"
+        />
+        How sunny was it today (0 being gloomy to 5 being sunniest)?
+        <RadioButtonsRow
+          counterType="counterFive"
+          clickHandler={this.handleCheck}
+          name="sun"
+        />
         Rate the overall pleasantness of your day:
         <RadioButtonsRow
           counterType="counterTen"
           handleClick={this.handleCheck}
         />
-        Rate the tension in your day ( excitement is a postive tension. low
-        energy high tension: dread. low pleasatness, high plesasantness: excited
-        low energy low tension: calmness high pleasantness. tiredness/bored low
-        pleasantness )?
+        Rate the tension in your day (for example, a lot of excitement is a
+        pleasant tension, and stress is an unpleasant tension):
+        {/* excitement is a postive tension. low energy high tension: dread. low pleasatness, high plesasantness: excited
+        low energy low tension: calmness high pleasantness. tiredness/bored low pleasantness ) */}
         <RadioButtonsRow
           counterType="counterTen"
           handleClick={this.handleCheck}
         />
-        Rate your general energy level for today (low energy unpleasant: tired.
-        high energy unpeasnt: anxious low energy pleasnat: calm. high energy
-        pleasant: ecited)
+        Rate your general energy level today:
         <RadioButtonsRow
           counterType="counterTen"
           handleClick={this.handleCheck}
