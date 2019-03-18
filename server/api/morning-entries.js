54aef89cb58b999fa257e92dec9a10ae1b5f3424
Ornella Friggit <44591279+friggito@users.noreply.github.com>
2019-03-18 17:11:00 -0400

    Integrate ml model (#8)
    * feat(integrate-ml-model): created a basic folder with the model file, a current model and the translator func
    
    * feat(integrate-brain-model): commented out some things not immediately necessary
    
    * feat(prediction): connecting ML to form/post route. not done.
    
    * feat(prediction): successfully sent predictions back to state, edited model to separate tension, energy, and pleasantness

--- server/api/morning-entries.js
+++ server/api/morning-entries.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 const {MorningEntry} = require('../db/models')
+const {moodNetwork, avgOfRuns} = require('../brain-model/brain-model')
+const jsonToBrainData = require('../brain-model/translator-funcs')
 module.exports = router
 
 // api/morning-entries
@@ -14,8 +16,14 @@ router.get('/', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   try {
-    const newMorningEntry = await MorningEntry.create(req.body)
-
+    const translatedData = jsonToBrainData(req.body)
+    const modelOutput = avgOfRuns(translatedData, moodNetwork, 100)
+    const newMorningEntry = await MorningEntry.create({
+      ...req.body,
+      pleasant: modelOutput.pleasant,
+      tension: modelOutput.tension,
+      energy: modelOutput.energy
+    })
     res.send(newMorningEntry)
   } catch (error) {
     next(error)
---
+++ server/brain-model/translator-funcs.js
@@ -0,0 +1,44 @@
+//MAIN TRANSLATE FUNC
+
+const jsonToBrainData = entry => ({
+  sleep: sleepTranslator(entry.sleep),
+  social: usualTranslator(entry.social),
+  meals: zeroToFourTranslator(entry.meals),
+  exercise: yesOrNoTranslator(entry.exercise),
+  work: zeroToFiveTranslator(entry.work),
+  relax: usualTranslator(entry.relax),
+  sun: zeroToFourTranslator(entry.sun)
+})
+
+// TRANSLATOR FUNCS
+const sleepTranslator = sleepStr => {
+  if (sleepStr === '0-2') return 0
+  if (sleepStr === '2-4') return 0.25
+  if (sleepStr === '4-6') return 0.5
+  if (sleepStr === '6-8') return 0.75
+  if (sleepStr === '8+') return 1
+}
+
+const usualTranslator = str => {
+  if (str === 'Not at all' || str === 'None at all') return 0
+  if (str === 'Less than usual') return 1 / 3
+  if (str === 'Usual amount') return 2 / 3
+  if (str === 'More than usual') return 1
+}
+
+const zeroToFourTranslator = num => {
+  num = Number(num)
+  return num / 4
+}
+
+const zeroToFiveTranslator = num => {
+  num = Number(num)
+  return num / 5
+}
+
+const yesOrNoTranslator = str => {
+  if (str === 'Yes') return 1
+  if (str === 'No') return 0
+}
+
+module.exports = jsonToBrainData
