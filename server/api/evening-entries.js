a74ccd274a02df660d5847109e07e3c8e1e5eb35
friggito <ornella.friggit@gmail.com>
2019-03-19 14:46:36 -0400

    feat(api-routes): set up api routes to post and get all evening entries

---
+++ server/api/evening-entries.js
@@ -0,0 +1,21 @@
+const router = require('express').Router()
+const {EveningEntry} = require('../db/models')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+  try {
+    const eveningEntries = await EveningEntry.findAll()
+    res.json(eveningEntries)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newEveningEntry = await MorningEntry.create(req.body)
+    res.send(newEveningEntry)
+  } catch (error) {
+    next(error)
+  }
+})
