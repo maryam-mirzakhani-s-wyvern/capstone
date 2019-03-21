99a70db2aa11e69598c59ecc6a3e4a7226eced35
Halim Chu <halim.chu@gmail.com>
2019-03-20 21:04:29 -0400

    incorporated weather api

--- server/api/index.js
+++ server/api/index.js
@@ -4,7 +4,7 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/morning-entries', require('./morning-entries'))
 router.use('/evening-entries', require('./evening-entries'))
-// router.use('/weather', require('./weather'))
+router.use('/weather', require('./weather'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
