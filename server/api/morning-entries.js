0c2adc0341614b2626773a9c072e21002dfa8a5c
Torri <31962308+rainyleaf@users.noreply.github.com>
2019-03-22 11:25:42 -0400

    Retraining model (#22)
    * feat(evening data) translates data from evening form into format digestible by NN
    
    * feat(model): model is trained on real data and re-trains from evening data
    
    * trying to resolve nonexistant merge conflict
    
    * fix(retraining-models): deleted bracket that was breaking everything

--- server/api/morning-entries.js
+++ server/api/morning-entries.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const {MorningEntry, User} = require('../db/models')
-const {moodNetwork, avgOfRuns} = require('../brain-model/brain-model')
-const jsonToBrainData = require('../brain-model/translator-funcs')
+const {moodNetwork} = require('../brain-model/brain-model')
+const {jsonToBrainData} = require('../brain-model/translator-funcs')
 module.exports = router
 
 // /api/morning-entries
@@ -24,56 +24,17 @@ router.get('/', async (req, res, next) => {
 // api/morning-entries
 router.post('/', async (req, res, next) => {
   try {
-    if (req.session.passport) {
-      const translatedData = jsonToBrainData(req.body)
-      const modelOutput = avgOfRuns(translatedData, moodNetwork, 100)
-      const newMorningEntry = await MorningEntry.create({
-        ...req.body,
-        pleasant: modelOutput.pleasant,
-        tension: modelOutput.tension,
-        energy: modelOutput.energy,
-        userId: req.session.passport.user
-      })
-      res.send(newMorningEntry)
-    } else {
-      const translatedData = jsonToBrainData(req.body)
-      const modelOutput = avgOfRuns(translatedData, moodNetwork, 100)
-      const newMorningEntry = await MorningEntry.create({
-        ...req.body,
-        filledOut: true,
-        pleasant: modelOutput.pleasant,
-        tension: modelOutput.tension,
-        energy: modelOutput.energy
-      })
-      res.send(newMorningEntry)
-    }
+    const translatedData = jsonToBrainData(req.body)
+    const modelOutput = moodNetwork.run(translatedData)
+    const newMorningEntry = await MorningEntry.create({
+      ...req.body,
+      pleasant: modelOutput.pleasant,
+      tension: modelOutput.tension,
+      energy: modelOutput.energy,
+      userId: req.session.passport.user
+    })
+    res.send(newMorningEntry)
   } catch (error) {
     next(error)
   }
 })
-
-// api/morning-entries
-// router.get('/', async (req, res, next) => {
-//   try {
-//     const morningEntries = await MorningEntry.findAll()
-//     res.json(morningEntries)
-//   } catch (error) {
-//     next(error)
-//   }
-// })
-
-// router.post('/', async (req, res, next) => {
-//   try {
-//     const translatedData = jsonToBrainData(req.body)
-//     const modelOutput = avgOfRuns(translatedData, moodNetwork, 100)
-//     const newMorningEntry = await MorningEntry.create({
-//       ...req.body,
-//       pleasant: modelOutput.pleasant,
-//       tension: modelOutput.tension,
-//       energy: modelOutput.energy
-//     })
-//     res.send(newMorningEntry)
-//   } catch (error) {
-//     next(error)
-//   }
-// })
