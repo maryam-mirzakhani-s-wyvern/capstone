875b50a665c82b03f7b3403530556b5a342bfe9c
friggito <ornella.friggit@gmail.com>
2019-03-22 15:09:01 -0400

    feat(navbar): also deleted the morning form link from front-end for guests

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -25,7 +25,6 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
-          <Link to="/morningform">Plan Your Day (FOR TESTING)</Link>
           <Link to="/eveningform">Reflect on Day (FOR DATA COLLECTION)</Link>
         </div>
       )}
