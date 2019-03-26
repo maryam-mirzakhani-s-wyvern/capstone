875187332b05e55fc6631fcbbf9358cc32b43a06
Torri Raines <raines.torri@gmail.com>
2019-03-26 19:49:08 -0400

    feat(navbar) has dropdown for account-related things

--- client/routes.js
+++ client/routes.js
@@ -47,7 +47,7 @@ class Routes extends Component {
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={UserHome} />
+            <Route path="/account" component={UserHome} />
             <Route exact path="/comparison" component={Comparison} />
           </Switch>
         )}
