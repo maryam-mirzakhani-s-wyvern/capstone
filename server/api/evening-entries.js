ea65f3f7f8bbd7ecee8fff365e0dcedd6e76a33f
Halim Chu <halim.chu@gmail.com>
2019-03-26 15:54:25 -0400

    fix(comparisonpage)

--- server/api/evening-entries.js
+++ server/api/evening-entries.js
@@ -3,8 +3,8 @@ const router = require('express').Router()
 const {EveningEntry} = require('../db/models')
 const {moodNetwork, savenet} = require('../brain-model/brain-model')
 const {jsontoTrainingData} = require('../brain-model/translator-funcs')
-
 module.exports = router
+
 //route protection function
 function checkAdmin(req, res, next) {
   if (req.user.isAdmin) {
@@ -15,10 +15,22 @@ function checkAdmin(req, res, next) {
     res.status(403).send('403 forbidden')
   }
 }
+
 router.get('/', checkAdmin, async (req, res, next) => {
   try {
     const eveningEntries = await EveningEntry.findAll()
     res.json(eveningEntries)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const userEntriesEve = await EveningEntry.findAll({
+      where: {userId: req.params.userId}
+    })
+    res.json(userEntriesEve)
   } catch (error) {
     next(error)
   }
@@ -39,33 +51,27 @@ router.get('/today', async (req, res, next) => {
   }
 })
 
-// HALIM'S CODE
-router.post('/', async (req, res, next) => {
-  const today = moment().format('YYYY-MM-DD')
-  try {
-    if (req.session.passport.user) {
-      const newEveningEntry = await EveningEntry.create({
-        ...req.body, //entryToPost
-        userId: req.session.passport.user,
-        date: today
-      })
-      // const trainingData = jsontoTrainingData(req.body)
-      // moodNetwork.train(trainingData)
-      // savenet(moodNetwork, './network.json')
-      res.send(newEveningEntry)
-    }
+// // HALIM'S CODE
+// router.post('/', async (req, res, next) =>  {
+//   try {
+//     const today = moment().format('YYYY-MM-DD')
+//     if (req.session.passport.user) {
+//       const newEveningEntry = await EveningEntry.create({
+//         ...req.body, //entryToPost
+//         userId: req.session.passport.user,
+//         date: today
+//       })
+//       res.send(newEveningEntry)
+//     }
+//       // const trainingData = jsontoTrainingData(req.body)
+//       // moodNetwork.train(trainingData)
+//       // savenet(moodNetwork, './network.json')
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
-router.get('/:userId', async (req, res, next) => {
-  try {
-    const userEntriesEve = await EveningEntry.findAll({
-      where: {userId: req.params.userId}
-    })
-    res.json(userEntriesEve)
-  } catch (error) {
-    next(error)
-  }
-})
-
+// HALIM-TORRI CODE
 router.post('/', async (req, res, next) => {
   try {
     let userId
@@ -79,7 +85,7 @@ router.post('/', async (req, res, next) => {
       date: new Date(),
       userId: userId
     })
-    const trainingData = jsontoTrainingData(req.body)
+    // const trainingData = jsontoTrainingData(req.body)
     //console.log("here's the trainingData: ", trainingData)
     //console.log("here's the network pre-training: ", moodNetwork.toJSON())
     //moodNetwork.train(trainingData)
@@ -90,3 +96,29 @@ router.post('/', async (req, res, next) => {
     next(error)
   }
 })
+
+// // TORRIS
+// router.post('/', async (req, res, next) => {
+//   try {
+//     let userId
+//     if (req.session.passport) {
+//       userId = req.session.passport.user
+//     } else {
+//       userId = null
+//     }
+//     const newEveningEntry = await EveningEntry.create({
+//       ...req.body,
+//       date: new Date(),
+//       userId: userId
+//     })
+//     // const trainingData = jsontoTrainingData(req.body)
+//     //console.log("here's the trainingData: ", trainingData)
+//     //console.log("here's the network pre-training: ", moodNetwork.toJSON())
+//     //moodNetwork.train(trainingData)
+//     //console.log("and here it is post-training: ", moodNetwork.toJSON())
+//     //savenet(moodNetwork, './network.json')
+//     res.send(newEveningEntry)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
