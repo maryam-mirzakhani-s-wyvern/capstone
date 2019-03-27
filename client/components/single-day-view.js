0bc4ca989e686baea32870a7dfbba12bd27d9d09
friggito <ornella.friggit@gmail.com>
2019-03-27 17:28:46 -0400

    feat(css): column breakups

--- client/components/single-day-view.js
+++ client/components/single-day-view.js
@@ -8,11 +8,11 @@ export default function SingleDay(props) {
       <h4>
         Day Summary: {moment(props.entry.createdAt).format('dddd MMM Do')}
       </h4>
-      <div className="col s12 l6">
+      <div className="col s12 m6 l6">
         <InputSummary input={props.entry} />
         <InputChart input={props.entry} />
       </div>
-      <div className="col s12 l6">
+      <div className="col s12 m6 l6">
         <Prediction
           tension={props.entry.tension}
           pleasant={props.entry.pleasant}
