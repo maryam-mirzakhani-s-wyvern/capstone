2036a2581b89e6355028741c499be0fa0b618c32
Dani Jordan <dandelionwalk@gmail.com>
2019-03-28 15:24:13 -0400

    feat(404): displays message for 404 error (#53)

---
+++ client/components/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = props => {
+  return (
+    <div>
+      <h2>404: Not Found</h2>
+      <p>Sorry, I'm pretty sure this is not the page you are looking for.</p>
+    </div>
+  )
+}
+
+export default NotFound
