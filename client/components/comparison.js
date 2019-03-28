2f4110f7026fec144167f477e45428639872983e
Halim Chu <halimchu@users.noreply.github.com>
2019-03-28 11:43:50 -0400

    Comparisonpagefamilynight (#55)
    * feat(comparison page) not really working
    
    * feat(comparisonpage) cleaned out the code for friends and family night
    
    * feat(comparisonpage) cleaned up code
    
    * Update comparison.js
    
    * Delete data.txt

--- client/components/comparison.js
+++ client/components/comparison.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import MoodRadialChart from './mood-radial-chart'
+import SingleInputChart from './single-input-chart'
 import {fetchThisMorning} from '../store/morning-entry'
 import {fetchThisEvening} from '../store/evening-entry'
 
@@ -10,19 +11,18 @@ class Comparison extends Component {
   }
 
   componentDidMount() {
-    const userId = this.props.eveningEntries.userId
-    this.props.fetchEvening(userId)
+    this.props.fetchEvening()
     this.props.fetchMorning()
   }
 
   render() {
     const {morningEntries, eveningEntries} = this.props
-    console.log('comparison Props', this.props)
-    console.log('USERID', this.props.eveningEntries.userId)
+
     return (
       <div className="row center-align">
         <div className="col s6">
-          <h4 className="poiret underline">Morning</h4>
+          <h4 className="poiret underline">Expected Output</h4>
+          <SingleInputChart input={morningEntries} />
           {this.props.morningEntries.id && this.props.morningEntries.id ? (
             <MoodRadialChart
               tension={morningEntries.tension}
@@ -35,7 +35,8 @@ class Comparison extends Component {
         </div>
 
         <div className="col s6">
-          <h4 className="poiret underline">Evening</h4>
+          <h4 className="poiret underline">Actual Output</h4>
+          <SingleInputChart input={eveningEntries} />
           {this.props.eveningEntries.id && this.props.eveningEntries.id ? (
             <MoodRadialChart
               tension={eveningEntries.actualtension}
@@ -51,10 +52,12 @@ class Comparison extends Component {
   }
 }
 
-const mapState = state => ({
-  eveningEntries: state.eveningEntry.postedEntry,
-  morningEntries: state.morningEntry.postedEntry
-})
+const mapState = state => {
+  return {
+    eveningEntries: state.eveningEntry.postedEntry,
+    morningEntries: state.morningEntry.postedEntry
+  }
+}
 
 const mapDispatch = dispatch => ({
   fetchMorning: () => dispatch(fetchThisMorning()),
